f7466b195a250737df1411e4cbc62dcfc3e6d23c
nemanja2799 <nemanjaivanovic2799@gmail.com>
2024-06-11 12:31:08 +0200

    Kreirani dugmici za poznavanje reci(known and unknown).

--- main.py
+++ main.py
@@ -13,12 +13,21 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas = Canvas(width=800, height=526)
 canvas.create_image(400, 263, image=card_front_img)
-canvas.grid(row=0, column=0)
+canvas.grid(row=0, column=0,columnspan=2)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 
 # text create
 canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-
 canvas.create_text(400,263, text="word", font=("ariel", 60, "bold"))
+
+# wrong button
+wrong_button_img = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=wrong_button_img, highlightthickness=0)
+unknown_button.grid(row=1, column=0)
+
+# right button
+right_button_img = PhotoImage(file="images/right.png")
+known_button = Button(image=right_button_img, highlightthickness=0)
+known_button.grid(row=1, column=1)
 
 window.mainloop()
