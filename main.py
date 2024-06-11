6dd0c8f013317e5741bcb777a8f60369acc7a403
nemanja2799 <nemanjaivanovic2799@gmail.com>
2024-06-11 16:15:02 +0200

    Uvedena promena reci na engleski, nakon 3 sekunde od prikaza reci na francuskom.Tada se menja boja kartice i fonta takodje. Nakon svakog klika na dugme krece novo odbrojavanje od 3 sekunde pre sledece promene reci.

--- main.py
+++ main.py
@@ -5,11 +5,20 @@ import random
 
 data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
-print(to_learn)
+current_card = {}
 def next_card():
+    global current_card, flip_timmer
+    window.after_cancel(flip_timmer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_image, image=card_front_img)
+    flip_timmer = window.after(3000, func=flip_card)
+
+def flip_card():
+    canvas.itemconfig(card_image, image=card_back_img)
+    canvas.itemconfig(card_title,text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
 
 
 # window create
@@ -17,12 +26,16 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timmer = window.after(3000, func=flip_card)
+
 # image create
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas = Canvas(width=800, height=526)
-canvas.create_image(400, 263, image=card_front_img)
+card_image = canvas.create_image(400, 263, image=card_front_img)
 canvas.grid(row=0, column=0,columnspan=2)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+
+card_back_img = PhotoImage(file="images/card_back.png")
 
 # text create
 card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
